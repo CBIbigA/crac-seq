f59ac0732e08fc7bb3c255fba5558ca1304af90a
rochevin <vincent.rocher.armd@gmail.com>
2023-01-25 10:46:16 +0100

    change function to use less ram

--- workflow/scripts/RemoveDuplicates_UMI.R
+++ workflow/scripts/RemoveDuplicates_UMI.R
@@ -4,15 +4,12 @@ suppressWarnings(suppressMessages(library(tidyverse)))
 dataset_raw <- snakemake@input[[1]]
 message(dataset_raw)
 
-myBam <- scanBam(dataset_raw)[[1]]
-
-myBam_seq_name <- tibble(names = myBam[["qname"]],seq = as.character(myBam[["seq"]]))
-
-myBam_tibble <- GAlignments(seqnames =myBam[["rname"]],pos = myBam[["pos"]],cigar = myBam[["cigar"]],
-                        strand = myBam[["strand"]],names = myBam[["qname"]])%>% as.data.frame() %>% tibble::rownames_to_column("names")  %>% as_tibble()
+BF <- BamFile(dataset_raw) ;
+param <- ScanBamParam(what="seq")
 
 
-myBam_tibble <- myBam_tibble %>% left_join(myBam_seq_name)
+myBam_tibble <- readGAlignments(BF,use.names = T, param=param) %>% as.data.frame() %>% tibble::rownames_to_column("names")  %>% as_tibble()
+
 myBam_tibble <- myBam_tibble %>% mutate(UMI = str_extract(names,"[ACGT]+$"))
 
 stats <- myBam_tibble %>% dplyr::count(start,end,strand,UMI,seq) 
