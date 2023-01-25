00617834914f9dd85caca347032f475972347cd3
rochevin <vincent.rocher.armd@gmail.com>
2023-01-25 10:39:35 +0100

    change UMI detection in script

--- workflow/scripts/RemoveDuplicates_UMI.R
+++ workflow/scripts/RemoveDuplicates_UMI.R
@@ -10,8 +10,10 @@ myBam_seq_name <- tibble(names = myBam[["qname"]],seq = as.character(myBam[["seq
 
 myBam_tibble <- GAlignments(seqnames =myBam[["rname"]],pos = myBam[["pos"]],cigar = myBam[["cigar"]],
                         strand = myBam[["strand"]],names = myBam[["qname"]])%>% as.data.frame() %>% tibble::rownames_to_column("names")  %>% as_tibble()
+
+
 myBam_tibble <- myBam_tibble %>% left_join(myBam_seq_name)
-myBam_tibble <- myBam_tibble %>% mutate(UMI = str_extract(names,"[ACGT]{5}$"))
+myBam_tibble <- myBam_tibble %>% mutate(UMI = str_extract(names,"[ACGT]+$"))
 
 stats <- myBam_tibble %>% dplyr::count(start,end,strand,UMI,seq) 
 res <- myBam_tibble %>% dplyr::group_by(start,end,strand,UMI,seq) %>% slice(1) %>% pull(names)
